a430823b2a96cb5095b0e670c095c52081aafd08
Andrei Csatlos <acsatlos@192-168-0-177.rdsnet.ro>
2023-04-28 22:57:02 +0300

    Initial commit

---
+++ app/ios/Pods/Target Support Files/Pods-ElectricVehicleSharingApp-ElectricVehicleSharingAppTests/Pods-ElectricVehicleSharingApp-ElectricVehicleSharingAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ElectricVehicleSharingApp_ElectricVehicleSharingAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_ElectricVehicleSharingApp_ElectricVehicleSharingAppTests
+@end
