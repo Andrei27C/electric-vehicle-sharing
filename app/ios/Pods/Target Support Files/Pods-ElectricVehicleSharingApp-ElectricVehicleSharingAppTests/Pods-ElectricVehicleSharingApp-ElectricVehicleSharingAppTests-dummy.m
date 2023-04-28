4b18b400d9fceb1a8254f7084c96eae8d4584f9a
Andrei Csatlos <acsatlos@192-168-0-177.rdsnet.ro>
2023-04-28 23:06:25 +0300

    Remove large files from the Git index

--- app/ios/Pods/Target Support Files/Pods-ElectricVehicleSharingApp-ElectricVehicleSharingAppTests/Pods-ElectricVehicleSharingApp-ElectricVehicleSharingAppTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ElectricVehicleSharingApp_ElectricVehicleSharingAppTests : NSObject
-@end
-@implementation PodsDummy_Pods_ElectricVehicleSharingApp_ElectricVehicleSharingAppTests
-@end
